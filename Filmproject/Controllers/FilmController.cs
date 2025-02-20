b7244ab2b3c923d883e8612fee7ac26a493885ea
Niklas Karlsson <shadowblade83@hotmail.com>
2025-02-20 19:50:32 +0100

    Add project files.

---
+++ Filmproject/Controllers/FilmController.cs
@@ -0,0 +1,34 @@
+﻿using Filmproject_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filmproject_2.Controllers
+{
+    public class FilmController : Controller
+    {
+        // GET: Film
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult VisaFilm(int id)
+        {
+            Film nyFilm = new Film();
+            if (id == 1)
+            {
+                nyFilm.Titel = "Lejonkungen";
+                nyFilm.Åldersgräns = 7;
+            }
+            else if (id == 2)
+            {
+                nyFilm.Titel = "Toy Story 4";
+                nyFilm.Åldersgräns = 3;
+            }
+            return View(nyFilm);
+        }
+    }
+}
---
+++ Filmproject/Models/Film.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Filmproject_2.Models
+{
+    public class Film
+    {
+        public string Titel { get; set; }
+        public int Åldersgräns { get; set; }
+        public string Regissör { get; set; }
+    }
+}
